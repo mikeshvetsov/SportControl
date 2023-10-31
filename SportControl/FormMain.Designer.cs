e9a21fa71f1d96ddefe81ad9317d5a4b479ab0d7
mikeshvetsov <46900808+mikeshvetsov@users.noreply.github.com>
2023-10-31 22:49:37 +0300

    переименование закладок

--- SportControl/FormMain.Designer.cs
+++ SportControl/FormMain.Designer.cs
@@ -115,7 +115,7 @@ namespace SportControl
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(1342, 622);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Регистрация";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // flowLayoutPanel_Edit
@@ -304,9 +304,9 @@ namespace SportControl
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1342, 599);
+            this.tabPage2.Size = new System.Drawing.Size(1342, 622);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Заед";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
@@ -321,9 +321,9 @@ namespace SportControl
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1342, 599);
+            this.tabPage3.Size = new System.Drawing.Size(1342, 622);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.Text = "Настройки";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // groupBox_ant
